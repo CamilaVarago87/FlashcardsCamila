c24e5a6b20f77ed7a012d830644a2663cc7f891d
CamilaVarago87 <127787215+CamilaVarago87@users.noreply.github.com>
2024-11-28 21:54:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Matemática',
+    'O que são polígonos regulares?',
+    'São polígonos cujos lados e ângulos são congruentes'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o nome da linha que divide um círculo em duas partes iguais?',
+    'Diâmetro'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o nome da figura geométrica que tem cinco lados?',
+    'Pentágono'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o nome da figura geométrica que possui seis lados?',
+    'Hexágono'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o nome da figura geométrica que possui quatro lados iguais e quatro ângulos retos?',
+    'Quadrado'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o resultado de 25% de 80?',
+    '20'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o nome da figura geométrica que possui sete lados?',
+    'Heptágono'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o nome da figura geométrica que possui oito lados?',
+    'Octógono'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o nome da figura geométrica que possui dez lados?',
+    'Decágono'
+)
+
